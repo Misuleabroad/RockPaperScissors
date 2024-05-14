ba53bab4f2063f029d082b3213521567b7de7b7f
Misuleabroad <michaela.cermakova41@gmail.com>
2024-05-14 10:18:58 +0100

    finished playRound function
    added a result comparison function playRound

--- myScript.js
+++ myScript.js
@@ -1,12 +1,45 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-  let computerChoice = Math.random();
-  if (computerChoice <= 0.33) {
+  let computerRound = Math.floor(Math.random()) * 3;
+  if (computerRound == 1) {
     return "rock";
-  } else if (computerChoice >= 0.34 && computerChoice < 0.66) {
+  } else if (computerRound == 2) {
     return "paper";
   } else {
     return "scissors";
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let humanRound = prompt("Rock, Paper or Scissors? ").toLowerCase();
+  if (humanRound == "rock") {
+    return "rock";
+  } else if (humanRound == "paper") {
+    return "paper";
+  } else if (humanRound == "scissors") {
+    return "scissors";
+  } else {
+    alert("enter 'rock, paper or scissors'");
+  }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+console.log(computerSelection);
+console.log(humanSelection);
+
+function playRound(computerSelection, humanSelection) {
+  if (
+    (computerSelection == "paper" && humanSelection == "rock") ||
+    (computerSelection == "scissors" && humanSelection == "paper") ||
+    (computerSelection == "rock" && humanSelection == "scissor")
+  ) {
+    return `You lose ${computerSelection} beats ${humanSelection}`;
+  } else if (computerSelection === humanSelection) {
+    return `Draw`;
+  } else return `You win`;
+}
+
+console.log(playRound(computerSelection, humanSelection));
