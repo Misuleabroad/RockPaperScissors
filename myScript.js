5f4689f8c51c5cb7b39e1e95737af6974827d671
Misuleabroad <michaela.cermakova41@gmail.com>
2024-05-17 14:12:10 +0100

    finished 5 rounds

--- myScript.js
+++ myScript.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  let computerRound = Math.floor(Math.random()) * 3;
+  let computerRound = Math.floor(Math.random() * 3);
   if (computerRound == 1) {
     return "rock";
   } else if (computerRound == 2) {
@@ -25,21 +25,42 @@ function getHumanChoice() {
   }
 }
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-console.log(computerSelection);
-console.log(humanSelection);
-
-function playRound(computerSelection, humanSelection) {
+function playRound() {
+  const computerSelection = getComputerChoice();
+  const humanSelection = getHumanChoice();
+  console.log(computerSelection);
+  console.log(humanSelection);
   if (
     (computerSelection == "paper" && humanSelection == "rock") ||
     (computerSelection == "scissors" && humanSelection == "paper") ||
     (computerSelection == "rock" && humanSelection == "scissor")
   ) {
-    return `You lose ${computerSelection} beats ${humanSelection}`;
+    console.log(`You lose ${computerSelection} beats ${humanSelection}`);
+    computerScore++;
   } else if (computerSelection === humanSelection) {
-    return `Draw`;
-  } else return `You win`;
+    console.log(`Draw`);
+  } else console.log(`You win`);
+  humanScore++;
 }
 
-console.log(playRound(computerSelection, humanSelection));
+function playGame() {
+  let i = 0;
+  while (i < 5) {
+    i++;
+    playRound();
+  }
+}
+
+function winner() {
+  if (humanScore < computerScore) {
+    console.log(
+      `Computer is the winner. Computer score: ${computerScore} Player: ${humanScore}`
+    );
+  } else
+    console.log(
+      `You are the winner. Computer score: ${computerScore} Player: ${humanScore}`
+    );
+}
+
+playGame();
+winner();
